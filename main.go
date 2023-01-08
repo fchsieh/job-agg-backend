cfd8ba911540628ae0798e55435366972757ac70
Fred Hsieh <fhsieh@uci.edu>
2023-01-07 16:21:58 -0800

    fix: CORS middleware for router

--- main.go
+++ main.go
@@ -25,10 +25,25 @@ func read_config() config.Config {
 	return conf
 }
 
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 func set_router(conf config.Config) *gin.Engine {
 	fb := database.FirebaseInit(conf)
 	mongo := database.MongoInit(conf)
 	router := gin.Default()
+	router.Use(CORS())
 
 	router.GET("/jobs", func(c *gin.Context) {
 		routes.GetJobs(c, fb, mongo)
